6b313d9539575a6b063c101711d505f0d3b3a93f
ROHITH ALLA <44164350+rohithalla@users.noreply.github.com>
2020-10-02 19:57:53 +0530

    updates aws file

--- aws.py
+++ aws.py
@@ -13,25 +13,25 @@ t2.to_csv('t2.csv')
 s3 = boto3.resource(
     service_name='s3',
     region_name='us-east-2',
-    aws_access_key_id=' AKIAWI34OZCUG2C4QDNE',
-    aws_secret_access_key='5XfdMpu1QS7AOgvq5vkcaEupEivuUgFrxL9ma7fQ'
+    aws_access_key_id='your aws access key id',
+    aws_secret_access_key='your aws secret access key'
 )
 
 # Print out bucket names
 #for bucket in s3.buckets.all():
 #    print(bucket.name)
 
-#s3.Bucket('rohithalto').upload_file(Filename='t1.csv', Key='t1.csv')
-#s3.Bucket('rohithalto').upload_file(Filename='t2.csv', Key='t2.csv')
+#s3.Bucket('your bucket name').upload_file(Filename='t1.csv', Key='t1.csv')
+#s3.Bucket('your bucket name').upload_file(Filename='t2.csv', Key='t2.csv')
 
-for obj in s3.Bucket('rohithalto').objects.all():
+for obj in s3.Bucket('your bucket name').objects.all():
     print(obj)
 
 # Load csv file directly into python
-obj = s3.Bucket('rohithalto').Object('t1.csv').get()
+obj = s3.Bucket('your bucket name').Object('t1.csv').get()
 foo = pd.read_csv(obj['Body'], index_col=0)
 print(foo)
 
 # Download file and read from disc
-#s3.Bucket('cheez-willikers').download_file(Key='foo.csv', Filename='foo2.csv')
+#s3.Bucket('your bucket name').download_file(Key='foo.csv', Filename='foo2.csv')
 #pd.read_csv('foo2.csv', index_col=0)
